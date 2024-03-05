3480b5d266cd44c069a228e4f96e4134e11dff37
DevStark <starkji555@gmail.com>
2024-03-05 10:03:09 +0530

    Completed UserRecipe Page

--- client/src/pages/UserRecipe.jsx
+++ client/src/pages/UserRecipe.jsx
@@ -5,12 +5,14 @@ import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
 import axios from "axios";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
   const [recipe, setRecipe] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   const params = useParams();
   useEffect(() => {
     const fetchListing = async () => {
@@ -47,7 +49,7 @@ export default function Listing() {
           <Swiper navigation>
             {recipe.imageUrls.map((url) => (
               <SwiperSlide key={url}>
-                <div  
+                <div
                   className="h-[550px]"
                   style={{
                     background: `url(${url}) center no-repeat`,
@@ -57,6 +59,32 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="fixed top-[18%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          {recipe && (
+            <div>
+              <p className="p-5 text-3xl font-semibold">{recipe.title} - {recipe.category}</p>
+              <span className="font-semibold">Description:-</span>
+              <p className="p-2">{recipe.description}</p>
+              <span className="font-semibold pt-4">Instructions</span>
+              <p className="p-2">{recipe.instructions}</p>
+            </div>
+          )}
         </div>
       )}
     </main>
