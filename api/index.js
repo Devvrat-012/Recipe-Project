daaf2f933d55c6adc1cb8e0d90447315a40cbe6c
DevStark <starkji555@gmail.com>
2024-06-06 13:45:07 +0530

    Some configuration change

--- api/index.js
+++ api/index.js
@@ -23,6 +23,16 @@ mongoose
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://recipe-project-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cors({
   origin:"https://recipe-project-client.vercel.app",
   credentials:true
